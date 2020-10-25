045804b09a0fdfba547b5bc5984ff24e017f4e51
ksarshaeva <sarshaevak@gmail.com>
2020-10-25 21:46:40 +0600

    test commit

---
+++ jetpack.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+size=(1024,768)
+fps=25
+
+screen=pygame.display.set_mode(size)
+clock=pygame.time.Clok()
+
+
+done=False
+while not done:
+    for event in pygame.event.get():
+        if event.type==pygame.QUIT:
+            done=True
+
+
+    clock.tick(fps)
+
+    screen.display.flip()
+pygame.quit()
