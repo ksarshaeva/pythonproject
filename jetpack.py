3ecf2741f5aa6aaa944eb52e645990bd629975bf
ksarshaeva <sarshaevak@gmail.com>
2020-10-29 22:34:33 +0600

    working on shockers

--- jetpack.py
+++ jetpack.py
@@ -2,6 +2,7 @@ import pygame
 import random
 width = 1030
 height = 580
+floor = height - 50
 fps=60
 
 name = "spritesheet.png"
@@ -12,6 +13,11 @@ WHITE = (255,255,255)
 #initializing pygame and creating a window 
 pygame.init()
 pygame.mixer.init() #sounds
+"""
+pygame.font.init()
+welcome_font = pygame.font.SysFont('Comic Sans MS', 48)
+instruction_font = pygame.font.SysFont('Comic Sans MS', 22)
+"""
 screen=pygame.display.set_mode((width,height))
 pygame.display.set_caption("Jetpack") #changing naming of the window
 clock=pygame.time.Clock()
@@ -32,27 +38,39 @@ class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)#built in function,without which sprite will not work
         self.flying = False
+        self.alive = True 
+        #for animation
         self.looks = Spritesheet(name)
         self.current_frame = 0
         self.last_update = 0 #keeps track when the last sprite change happened 
         self.load_images()
         self.image = self.running_frame[0]#how that sprite looks like
+        
         self.rect = self.image.get_rect()#rectangle that incloses the sprite
-        self.rect.centerx = 145
-        self.rect.bottom = height-50
+        self.rect.centerx = 140
+        self.rect.bottom = floor
         self.speedy = 0
         self.speedx = 0
+
+    def death(self):
+        if not self.alive:
+            self.speedy = 0
+            self.speedx = 0
+
 
     def update(self):
         self.animate()
         keystate=pygame.key.get_pressed() #gives a bool list of keys that are down(pressed)
-        self.speedx = +2
-        self.rect.x += self.speedx
+        if player.rect.right >= width / 3:
+            self.speedx = 0
+        else:
+            self.speedx = +2.7
+            self.rect.x += self.speedx
         if keystate[pygame.K_w]:
-            self.speedy = -5  #flying up
+            self.speedy = -4  #flying up
             self.flying = True 
         elif not keystate[pygame.K_w]:
-            self.speedy += 0.4    #gravity fall
+            self.speedy += 0.2    #gravity fall
         self.rect.y += self.speedy
             
         #boundaries 
@@ -60,6 +78,8 @@ class Player(pygame.sprite.Sprite):
             self.rect.top = 100
         if self.rect.bottom > height-50: #same for the floor
             self.rect.bottom = height-50
+
+        if abs(self.rect.bottom - floor) < 10:
             self.flying = False
             
     def load_images(self):
@@ -108,22 +128,29 @@ class Player(pygame.sprite.Sprite):
                 self.current_frame = (self.current_frame +1) % len(self.flying_frame)
                 self.image = self.flying_frame[self.current_frame]
         
-    
 class Mob(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.image.load('rocket.png').convert()
         self.image.set_colorkey(BLACK)
         self.rect = self.image.get_rect()
-        self.rect.x=random.randrange(width+10)
-        self.rect.y=random.randrange(height-50,0,-10)
-        self.speedx=random.randrange(1,8)
+        self.rect.x = width+10 #making it appear from outside of our window
+        self.rect.y = random.randrange(height-50,0,-10) #range for the location on the y axis 
+        self.speed = random.randrange(3,8)
         
     def update(self):
-        self.rect.x-=self.speedx
-
-
+        self.rect.x -= self.speed
         
+class Shocker(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.w = random.randrange(100, 160, 10)
+        self.image = pygame.Surface((self.w, 35))
+        self.image.fill(WHITE)
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(width, width + 100, 10)
+        self.rect.y = random.randrange(height-75,90,-10)
+    
 class Background():  #to move background with camera
       def __init__(self):
           self.background = pygame.image.load('fon.png').convert()
@@ -134,23 +161,37 @@ class Background():  #to move background with camera
 
           self.bgY2 = 0
           self.bgX2 = self.background_rect.width
-
-          self.moving_speed = 5
          
       def update(self):
-        #self.moving_speed+=1
-        self.bgX1 -= self.moving_speed
-        self.bgX2 -= self.moving_speed
+
+        if player.rect.right >= width / 3:
+            #moving shockers
+            for shocker in shockers:
+                shocker.rect.left -= 2.7
+                #deleting the shocker that moved off the screen 
+                if shocker.rect.right <= 0:
+                    shocker.kill()
+        #moving the background picture
+            self.bgX1 -= 2.7
+            self.bgX2 -= 2.7
         if self.bgX1 <= -self.background_rect.width:
             self.bgX1 = self.background_rect.width
         if self.bgX2 <= -self.background_rect.width:
             self.bgX2 = self.background_rect.width
-             
+
       def render(self):
          screen.blit(self.background, (self.bgX1, self.bgY1))
          screen.blit(self.background, (self.bgX2, self.bgY2))
 
+
 all_sprites = pygame.sprite.Group()
+shockers = pygame.sprite.Group() #group to hold all shockers, to do collisions
+if not shockers:
+    for i in range(2):
+        s = Shocker()
+        all_sprites.add(s)
+        shockers.add(s)
+        
 mobs=pygame.sprite.Group()
 player = Player() #drawing the player
 background=Background()
@@ -162,17 +203,32 @@ for i in range(2):
 
 
 
-running = True 
-while running:
 
+#waiting = True 
+running = True
+while running:
+   
     #keep loop running at the right speed
     clock.tick(fps)
-    
-    #process input 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-           
+            """
+            waiting = False
+        if event.type == pygame.K_SPACE:
+            waiting = False
+               """ 
+    #process input 
+    #приветствующее окно не убирается при нажатии пробела,  игра замораживается и "(He отвечает)"
+        """
+    while waiting:
+        screen.fill(WHITE)
+        game_name = welcome_font.render("Jetpack", False, BLACK)
+        instruction1 = instruction_font.render("Press space to start", False, BLACK )
+        screen.blit(game_name,(int(width/2), int(height/4)))
+        screen.blit(instruction1,(int(width/2), int(height*3/4)))
+        pygame.display.flip()
+    """       
                 
     #update
     background.update()
